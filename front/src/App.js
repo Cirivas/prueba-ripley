bb81f74ab18cc4229d2c3eb4879d0a4ac5f361dd
Camilo Rivas <ignacio@Camilos-MBP.iptv.microsoft.com>
2019-07-13 16:06:49 -0400

    basic product list

--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Card, List } from "./design";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 100vh;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <List>
+        <Card
+          img={
+            "https://ripleycl.imgix.net/http%3A%2F%2Fs3.amazonaws.com%2Fimagenes-sellers-mercado-ripley%2F2019%2F05%2F23173521%2FiPhone-6s-Rose-Gold.jpg?w=270&h=220&bg=FFFFFF&ch=Width%2CSave-Data&auto=format%2Ccompress&trimcolor=FFFFFF&trim=color&fit=fillmax&ixlib=js-1.1.0&s=edf28d78e334585717b003acac2a6523"
+          }
+          title={"producto"}
+          body={"precio"}
+          onClick={() => console.log("card clicked")}
+        />
+      </List>
+    </Container>
   );
 }
 
