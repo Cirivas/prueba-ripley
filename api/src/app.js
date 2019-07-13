d94d87ce4a662147e99724152c1c0f3ec1947cd2
Camilo Rivas <ignacio@Camilos-MBP.iptv.microsoft.com>
2019-07-13 16:42:37 -0400

    retrieve products by partNumbers

--- api/src/app.js
+++ api/src/app.js
@@ -1,11 +1,24 @@
 import express from 'express';
+import request from 'request';
+import { skus } from './sku';
+import simple from './simple-api';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// Get all products (from a SKU list)
+app.get('/products', (_, res) => {
+  // Take ["a", "b", ...] to "a,b,..."
+  const partNumbers = skus.join(',');
+  const uri = simple().byPartNumbers(partNumbers);
+  // do request. (json: {} parse the response body automatically)
+  return request.get(uri, { json: {} }, (error, r, body) => {
+    if (error) {
+      return res.send(500); // Internal server error
+    }
+    return res.send(body);
+  });
 });
 
 app.listen(5000, () => {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 5000!');
 });
