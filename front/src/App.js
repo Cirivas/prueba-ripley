04fc314dceb5347b486f68c3cd329d21543a4432
Camilo Rivas <ignacio@Camilos-MBP.iptv.microsoft.com>
2019-07-13 22:55:42 -0400

    use react-router-dom in main App component

--- front/src/App.js
+++ front/src/App.js
@@ -1,45 +1,20 @@
 import React from "react";
-import { Card, List } from "./design";
 import styled from "styled-components";
+import { Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
 
 const Container = styled.div`
   height: 100vh;
 `;
 
 function App() {
-  const [products, setProducts] = React.useState(null);
-
-  // At component mount, load products
-  React.useEffect(() => {
-    fetch("http://localhost:5000/api/products")
-      .then(res => res.json())
-      .then(response => {
-        setProducts(response);
-      });
-  }, []);
-
   return (
-    <Container>
-      {!products ? (
-        <div>Loading</div>
-      ) : (
-        <List>
-          {(products || []).map(product => (
-            <Card
-              key={product.uniqueID}
-              img={`https:${product.fullImage}`}
-              title={product.name}
-              body={product.prices.formattedListPrice}
-              onClick={() =>
-                fetch(`http://localhost:5000/api/product/${product.partNumber}`)
-                  .then(res => res.json())
-                  .then(res => console.log(res))
-              }
-            />
-          ))}
-        </List>
-      )}
-    </Container>
+    <Switch>
+      <Container>
+        <Route exact path="/" component={Home} />
+        <Route path="*" component={Home} />
+      </Container>
+    </Switch>
   );
 }
 
