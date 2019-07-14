dd9f6ce8ca785ee96a8adc3e8220e2f0776f5c49
Camilo Rivas <ignacio@Camilos-MBP.iptv.microsoft.com>
2019-07-14 13:02:21 -0400

    basic redis client

---
+++ src/redis-client.js
@@ -0,0 +1,29 @@
+import redis from 'redis';
+
+const TTL = 60;
+
+// Use default redis://127.0.0.1/6379
+const client = redis.createClient();
+client.on('connect', () => {
+  console.log('connected to redis client');
+});
+
+// Store a single key in redis.
+// Keys will be the product partNumber
+export const setKey = (key, value) => {
+  // Set an expiring time for key
+  client.set(key, value, () => {
+    client.expire(key, TTL);
+  });
+};
+
+// Store multiple keys based on array.
+// values element must contain partNumber property.
+export const setKeys = (values = []) => {
+  values.forEach((value) => {
+    const parsed = JSON.stringify(value);
+    setKey(value.partNumber, parsed);
+  });
+};
+
+export default client;
