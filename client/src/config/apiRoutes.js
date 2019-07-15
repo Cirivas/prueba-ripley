143deb374976d99e23912f278db509da902f9019
Camilo Rivas <ignacio@Camilos-MBP.iptv.microsoft.com>
2019-07-14 22:42:06 -0400

    added route to get visited elements

--- client/src/config/apiRoutes.js
+++ client/src/config/apiRoutes.js
@@ -2,5 +2,8 @@ export default {
   products: {
     all: () => "/api/products",
     byPartNumber: partNumber => `/api/products/${partNumber}`
+  },
+  visited: {
+    all: () => "/api/visited"
   }
 };
